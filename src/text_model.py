5613653a5a02e15f12fdb3806ae9dc6c15a10c1c
Oisín <oinolan@tcd.ie>
2020-12-20 18:09:08 +0000

    Added 3d cross validation

---
+++ src/text_model.py
@@ -0,0 +1,23 @@
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.neighbors import KNeighborsRegressor
+import testbench
+import chess_utils
+import numpy as np
+
+def games_to_opening_vecs(games):
+    return np.array(list(map(lambda game: chess_utils.game_to_vec(game, 35), games)))
+
+def gaussian_kernel(distances):
+    weights = np.exp(-75 * (distances ** 2))
+    return weights / np.sum(weights)
+
+knn_model = KNeighborsRegressor(n_neighbors = 12, weights = gaussian_kernel, metric='hamming')
+knn_pipe = Pipeline([
+    ('Game to vec', FunctionTransformer(games_to_opening_vecs)),
+    ('kNN', knn_model)
+])
+
+# 2 hyperparams to tune: # moves considered in game2vec; # neighbours considered
+
+testbench.test(knn_pipe, 10000, 1000, 'game2vec_gaussian_knn_report', 'Added gaussian weigting to knn after seeing promising results from cross-validation')
